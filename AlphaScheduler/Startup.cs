3d329c4ccfdefa3f6c3e724b1a2c57ece4be170f
David Polania <davidpolaniaac@users.noreply.github.com>
2016-02-25 20:30:28 -0500

    Commit Inicial

---
+++ AlphaScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlphaScheduler.Startup))]
+namespace AlphaScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
