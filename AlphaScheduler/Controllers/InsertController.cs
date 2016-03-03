894b4f289c9902b62fd55cc6e78ec0dd9e9318c6
David Polania <davidpolaniaac@users.noreply.github.com>
2016-03-03 06:41:04 -0500

    Diseno de formulario, validacion,busquedad vista de cada formulario

---
+++ AlphaScheduler/Controllers/InsertController.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlphaScheduler.Models;
+
+namespace AlphaScheduler.Controllers
+{
+    public class InsertController : Controller
+    {
+        // GET: /Insert/
+        public ActionResult RegisterSede(Models.Sede model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Sede.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+
+        }
+
+        public ActionResult RegisterFacultad(Models.Facultad model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Facultad.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+
+        public ActionResult RegisterPrograma(Models.Programa model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Programa.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+
+
+        }
+
+        public ActionResult RegisterLinea(Models.Linea model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Linea.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+        public ActionResult RegisterPensum(Models.Pensum model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Pensum.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+        public ActionResult RegisterMateria(Models.Materia model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Materia.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+
+        public ActionResult RegisterInsertMateria(Models.Pensum_Materia model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Pensum_Materia.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+
+
+        public ActionResult RegisterInsertModulo(Models.Modulo model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Modulo.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+
+
+        public ActionResult RegisterProfesor(Models.Profesor model, String postgrado_request)
+        {
+            var save = false;
+            using (var context = new DBALPHAEntities())
+            {
+                context.Profesor.Add(model);
+                try
+                {
+                    context.SaveChanges();
+                    save = true;
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (save)
+            {
+                return Content(postgrado_request + "({'status':'success'});");
+            }
+            else
+            {
+                return Content(postgrado_request + "({'status':'error'});");
+            }
+        }
+    }
+}
---
+++ AlphaScheduler/Scripts/Proyecto/Linea.js
@@ -0,0 +1,103 @@
+﻿function enviar(form) {
+    if (form.valid()) {
+        var formData = $('#formContainer').serialize();
+        console.log(formData);
+        jQuery.ajax({
+            type: 'POST',
+            contentType: "application/json",
+            url: 'Insert/RegisterLinea/',
+            crossDomain: true,
+            data: formData,
+            dataType: 'jsonp',
+            jsonp: 'postgrado_request',
+            async: true,
+            success: function (data) {
+
+                if (data.status == 'success') {
+
+                    alert("Datos guardados");
+
+                } else {
+                    alert("No se puede guardar");
+                }
+            },
+            error: function () {
+                alert("No se puede comunicar con el servidor");
+            }
+        });
+    }
+}
+
+
+
+$(document).ready(function () {
+
+    $('#tableDetails').dataTable({
+        "order": [[0, "desc"]]
+    });
+
+    form = $("#formContainer");
+
+    form.validate({
+        debug: true,
+        rules: {
+            Nombre: {
+                required: true,
+                minlength: 2,
+
+            },
+            Codigo: {
+                required: true,
+                minlength: 2,
+
+            }
+        }
+    });
+
+    $("#btnEnviar").on('click', function (event) {
+        event.preventDefault();
+
+        enviar(form);
+    });
+
+    $('#sede').on('change', function (event) {
+
+        var sele = $('#sede').val();
+        var text = '<span>Facultad </span>' +
+            '<select name="Id_facultad" id="Id_facultad">'+
+            '<option value="">Seleccionar</option>';
+        facultad = JSON.parse(facultad);
+        for (var i = 0; i < facultad.length; i++) {
+            if (facultad[i].FK_Id_Sede == parseInt(sele)) {
+                text += '<option value="' + facultad[i].Id_Facultad + '">' + facultad[i].Nombre + '</option>';
+            }
+        }
+
+        text += '</select>';
+
+        $('#facultad').html(text);
+
+    });
+
+    $('#facultad').on('change', function (event) {
+
+        var sele = $('#Id_facultad').val();
+        var text = '<span>Programa </span>' +
+            '<select name="FK_Id_Programa" id="FK_Id_Programa">' +
+            '<option value="">Seleccionar</option>';
+        programa = JSON.parse(programa);
+        for (var i = 0; i < programa.length; i++) {
+            console.log(programa[i].FK_Id_Sede + ' ' + parseInt(sele));
+            if (programa[i].FK_Id_Facultad == parseInt(sele)) {
+                text += '<option value="' + programa[i].Id_Programa + '">' + programa[i].Nombre + '</option>';
+            }
+        }
+
+        text += '</select>';
+        console.log(text);
+        $('#programa').html(text);
+
+    });
+
+
+});
