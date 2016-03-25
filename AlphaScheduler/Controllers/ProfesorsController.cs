865ba5023a83b94d3095a4ab4f7b0bafa93c542b
David Polania <davidpolaniaac@users.noreply.github.com>
2016-03-25 13:07:55 -0500

    Vistas y controladores para profesores

---
+++ AlphaScheduler/Controllers/ProfesorsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AlphaScheduler.Models;
+
+namespace AlphaScheduler.Controllers
+{
+    public class ProfesorsController : Controller
+    {
+        private DBALPHAEntities db = new DBALPHAEntities();
+
+        // GET: Profesors
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Profesor.ToListAsync());
+        }
+
+        // GET: Profesors/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = await db.Profesor.FindAsync(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profesors/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id_Profesor,Nombre,Email,FK_Id_Sede")] Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Profesor.Add(profesor);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(profesor);
+        }
+
+        // GET: Profesors/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = await db.Profesor.FindAsync(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesors/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id_Profesor,Nombre,Email,FK_Id_Sede")] Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profesor).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesors/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = await db.Profesor.FindAsync(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Profesor profesor = await db.Profesor.FindAsync(id);
+            db.Profesor.Remove(profesor);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
